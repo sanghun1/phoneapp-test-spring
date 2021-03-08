4cda627adf5da30618507dcac81894c80cb91620
sang hun <pooij96@naver.com>
2021-03-09 08:17:29 +0900

    android-test-spring

---
+++ src/main/java/com/cos/phoneapp/web/dto/CMRespDto.java
@@ -0,0 +1,14 @@
+package com.cos.phoneapp.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CMRespDto<T> {
+
+	private int code;
+	private T data;
+}
---
+++ src/main/java/com/cos/phoneapp/web/dto/PhoneReqDto.java
@@ -0,0 +1,14 @@
+package com.cos.phoneapp.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PhoneReqDto {
+
+	private String name;
+	private String tel ;
+}
